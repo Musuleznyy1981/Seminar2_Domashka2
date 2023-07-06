b2c0b95f7eaa86f4a161b1277f58694bdfbbd8cc
Maksim Musuleznyy <mycya2010@gmail.com>
2023-07-06 17:26:05 +0300

    Add file

---
+++ fractions.py
@@ -0,0 +1,61 @@
+# Вариант 1
+# Напишите программу, которая принимает две строки вида “a/b” - дробь с числителем и знаменателем.
+# Программа должна возвращать сумму и произведение дробей.
+# Для проверки своего кода используйте модуль fractions.
+from typing import Tuple, Any
+
+
+def efprocess_fractions (frac1_str, frac2_str):
+    # Преобразуем дроби из строк в числа
+    num1, denom1 = map(int, frac1_str.split("/"))
+    num2, denom2 = map(int, frac2_str.split("/"))
+
+    # Вычисляем сумму дробей
+    sum_frac_num = num1 * denom2 + num2 * denom1
+    sum_frac_denom = denom1 * denom2
+    sum_frac = (sum_frac_num, sum_frac_denom);
+
+    # Вычисляем произведение дробей
+    prod_frac_num = num1 * num2
+    prod_frac_denom = denom1 * denom2
+    prod_frac: tuple[Any, Any] = (prod_frac_num, prod_frac_denom)
+
+    return sum_frac, prod_frac
+
+# Пример использования функции
+frac1_str = "3/4"
+frac2_str = "2/3"
+
+sum_frac, prod_frac = efprocess_fractions(frac1_str, frac2_str)
+
+print(f"Сумма дробей {frac1_str} и {frac2_str} - {sum_frac[0]}/{sum_frac[1]}")
+print(f"Произведение дробей {frac1_str} и {frac2_str} - {prod_frac[0]}/{prod_frac[1]}")
+
+
+
+# Вариант 2
+
+import fractions
+
+first: str = str(input("Введите числитель первой дроби "))
+second: str = str(input("Введите знаменатель первой дроби "))
+third: str = str(input("Введите числитель второй дроби "))
+fourth: str = str(input("Введите знаменатель второй дроби "))
+number1: int = int(first)
+number2: int = int(second)
+number3: int = int(third)
+number4: int = int(fourth)
+
+
+result1 = (number1 / number2 ) + (number3 / number4)
+result2 = (number1 / number2 ) * (number3 / number4)
+print(result1)
+print(result2)
+firstfraction = fractions.Fraction(number1, number2)
+secondfraction = fractions.Fraction(number3, number4)
+result3 = firstfraction + secondfraction
+result4 = firstfraction * secondfraction
+print(result3)
+print(result4)
+
+
